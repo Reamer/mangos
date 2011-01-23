025ac44771c7235bcf3989bc765d6b91649761dd
Reamer <Philipp@.(none)>
2011-01-24 00:22:06 +0100

    fix shapeshift bugs

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -6466,9 +6466,6 @@ void Aura::HandleShapeshiftBoosts(bool apply)
         case FORM_SHADOW:
             spellId1 = 49868;
             spellId2 = 71167;
-
-            if(target->GetTypeId() == TYPEID_PLAYER)      // Spell 49868 have same category as main form spell and share cooldown
-                ((Player*)target)->RemoveSpellCooldown(49868);
             break;
         case FORM_GHOSTWOLF:
             spellId1 = 67116;
@@ -6485,10 +6482,14 @@ void Aura::HandleShapeshiftBoosts(bool apply)
     {
         if (spellId1)
         {
+            if(target->GetTypeId() == TYPEID_PLAYER)
+                ((Player*)target)->RemoveSpellCooldown(spellId1);
             target->CastSpell(target, spellId1, true, NULL, this );
         }
         if (spellId2)
         {
+            if(target->GetTypeId() == TYPEID_PLAYER)
+                ((Player*)target)->RemoveSpellCooldown(spellId2);
             target->CastSpell(target, spellId2, true, NULL, this);
         }
 
