f128e73432c25aba400b59121663a9f7590bfbe3
Reamer <Philipp@.(none)>
2011-05-22 13:36:48 +0200

    Improvment in Marks of Four Horsemen (naxxramas)

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2062,23 +2062,17 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                     case 28834:                             // Mark of Rivendare
                     case 28835:                             // Mark of Zeliek
                     {
-                         Aura* aur = target->GetAura(GetId(), EFFECT_INDEX_0);
-                         if (!aur)
-                             return;
-
-                         uint8 stacks = aur->GetStackAmount();
+                         uint32 stacks = GetStackAmount();
                          int32 damage = 0;
-
-                         if (stacks == 2)
-                            damage = 500;
-                         else if (stacks == 3)
-                            damage = 1500;
-                         else if (stacks == 4)
-                            damage = 4000;
-                         else if (stacks == 5)
-                            damage = 12500;
-                         else if (stacks > 5)
-                            damage = 20000 + 1000 * (stacks - 6);
+                         switch (stacks)
+                         {
+                            case 1: return;
+                            case 2: damage = 500;   break;
+                            case 3: damage = 1500;  break;
+                            case 4: damage = 4000;  break;
+                            case 5: damage = 12500; break;
+                            default: damage = 20000 + (1000 * (stacks - 6)); break;
+                         }
 
                          if (Unit* caster = GetCaster())
                               caster->CastCustomSpell(target, 28836, &damage, NULL, NULL, true, NULL, this, caster->GetGUID());
