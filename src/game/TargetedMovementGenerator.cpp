37762db8b0ce957f5c7a2a3b42d8f90ebdf38eff
Reamer <philipp.dallig@web.de>
2011-10-21 21:45:26 +0200

    recheck distance reset to 100

--- src/game/TargetedMovementGenerator.cpp
+++ src/game/TargetedMovementGenerator.cpp
@@ -159,7 +159,8 @@ bool TargetedMovementGeneratorMedium<T,D>::Update(T &owner, const uint32 & time_
     i_recheckDistance.Update(time_diff);
     if (i_recheckDistance.Passed())
     {
-        i_recheckDistance.Reset(50);
+        i_recheckDistance.Reset(100);
+
         //More distance let have better performance, less distance let have more sensitive reaction at target move.
         //float allowed_dist = owner.GetObjectBoundingRadius() + sWorld.getConfig(CONFIG_FLOAT_RATE_TARGET_POS_RECALCULATION_RANGE);
         float allowed_dist = owner.GetFloatValue(UNIT_FIELD_COMBATREACH) + i_target->GetFloatValue(UNIT_FIELD_COMBATREACH) - 1.0f;
