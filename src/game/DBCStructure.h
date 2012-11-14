27b83728a8b50a06d4503246da16152d3a935cb4
Reamer <philipp.dallig@web.de>
2012-11-14 23:02:12 +0100

    fix VMAPS problems

--- src/game/DBCStructure.h
+++ src/game/DBCStructure.h
@@ -1371,6 +1371,8 @@ struct MapEntry
 
     bool IsTransport() const
     {
+        if (IsContinent())
+            return false;
         return map_type == MAP_COMMON && mapFlags == MAP_FLAG_INSTANCEABLE;
     }
 };
